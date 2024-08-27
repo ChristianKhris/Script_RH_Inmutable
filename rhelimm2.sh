b8aaf3dac5e8c61e55499acc990d117b97ab1076
ChristianKhris <notalok4@gmail.com>
2024-08-27 10:55:47 -0600

    rhelimm2.sh

--- rhelimm2.sh
+++ rhelimm2.sh
@@ -14,9 +14,6 @@ lvcreate -l 100%FREE --name  repoimmsql
 mkfs.xfs -b size=4096 -m reflink=1,crc=1 /dev/repoimmsql/
 mkdir /repoveeamaql
 mount /dev/repoimmsql/repoveeamaql /repoveeamaql
-adduser veeamrepo
-echo "****** Please Enter veeamrepo Password ******"
-passwd veeamrepo
 mkdir /repoveeam/backupsql
 chown veeamrepo:veeamrepo /repoveeam/backupsql
 chmod 700 /repoveeam/backupsql
@@ -26,25 +23,25 @@ echo "******Saving /etc/fstab as /etc/fstab.$$******"
 echo "******Adding /repoveeamaql to /etc/fstab entry******"
 echo "UUID=${UUID} /repoveeamaql xfs defaults 1 1" >> /etc/fstab
 echo "******Please Add The New Repository with veeamrepo single-use credentiales in Veeam Backup & Replication******"
-while [ 1 ]
-do
-        pid=`ps -fea | grep "veeamimmureposvc" | grep -v grep`
-        echo $pid
-        if [ "$pid" = "" ]
-        then
-                echo "******Veeam Process is not here...******"
-                #exit
-        else
-                echo "******Veeam Process Detected continuing...******"
-                echo "******Denying SSH /etc/ssh/sshd_config entry******"
-                echo "DenyUsers veeamrepo" >> /etc/ssh/sshd_config
-                echo "******Disable SSH? Enter 1 for YES or 2 for NO******"
-                select yn in "Yes" "No"; do
-                case $yn in
-                Yes ) $(systemctl disable sshd && systemctl stop sshd); echo "SSH Service Disabled and Stopped, Please disconnect from SSH"; exit;;
-                No ) exit;;
-                esac
-                done
-                fi
-        sleep 8
-done
+#while [ 1 ]
+#do
+#        pid=`ps -fea | grep "veeamimmureposvc" | grep -v grep`
+#        echo $pid
+#        if [ "$pid" = "" ]
+#        then
+#                echo "******Veeam Process is not here...******"
+#                #exit
+#        else
+#                echo "******Veeam Process Detected continuing...******"
+#                echo "******Denying SSH /etc/ssh/sshd_config entry******"
+#                echo "DenyUsers veeamrepo" >> /etc/ssh/sshd_config
+#                echo "******Disable SSH? Enter 1 for YES or 2 for NO******"
+#                select yn in "Yes" "No"; do
+#                case $yn in
+#                Yes ) $(systemctl disable sshd && systemctl stop sshd); echo "SSH Service Disabled and Stopped, Please disconnect from SSH"; exit;;
+#                No ) exit;;
+#                esac
+#                done
+#                fi
+#        sleep 8
+#done
